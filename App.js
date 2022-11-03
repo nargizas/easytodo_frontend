4e6003c7bbdebabafbe8100841b73036110ad56b
nargizas <narwinner@gmail.com>
2022-11-03 18:54:10 +0900

    Add EmptyDashboard

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './screens/LoginScreen';
 import DashboardScreen from './screens/DashboardScreen';
+import EmptyDashboardScreen from './screens/EmptyDashboardScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Dashboard" component={DashboardScreen} />
+          <Stack.Screen name="EmptyDashboard" component={EmptyDashboardScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
